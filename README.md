83f839c098a589e7f0a1945bb3be3f44dcb42c4a
Cleyam <cleyam@gmail.com>
2020-02-03 15:38:38 +0100

    03/02/2020

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-TableBuilderComponent
+# **PHP Table Builder by Cleyam**
+
+This PHP class will help you easily create HTML tables with two methods.
+
+## **How to setup**
+
+* Copy the /vendor directory inside your project.
+
+
+## **Working with the Form Validator**
+
+To validate a form :
+
+1-  Require/Include/Autoload the TableBuilder class in your file.
+
+2-	Instantiate a new object with the TableBuilder class wherever you want in your HTML, within PHP tags. You can set an `id` and css `class` for said table in the parameters, which are empty by default.
+
+Exemple
+```php
+$newTable = new TableBuilder('id', 'css classes');
+```
+ 
+3-	For each column you need in the table, use the `newCol()` method to set the values you want to use, then when you are done, echo the table via the `createTable()` method.
+
+Exemple
+```php
+    $newTable->newColumn('name', ['Johnson', 'Doe', 'Spencer']);
+    $newTable->newColumn('surname', ['David', 'John', 'Phill']);
+    $newTable->newColumn('age', ['25', '28', '42']);
+    echo $newTable->createTable();
+```
+
+
+## **Methods**
+
+* #### newCol
+This method will automatically setup a new key/values array inside the $table attribute of the class. It will later be used to generate the table.
+
+`colName` - The name of your column, which will become the <tr> tag in your table <theader>.
+
+`colValues` - The values of the column, which will become the <td> tags in your table <tbody>.
+
+
+* #### createTable
+This method will return the HTML table with all the values previously setup with newCol(). You can use an echo to display it in your HTML.
+
---
+++ vendor/tableBuilder/tableBuilder.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace TableBuilder;
+
+class TableBuilder
+{
+    protected $table = [];
+    protected $tableId = "";
+    protected $tableClass = "";
+
+    public function __construct($id = "", $class = "")
+    {
+        $this->setTableId($id);
+        $this->setTableClass($class);
+    }
+    /**
+     * Create a new column in the table using the first parameter to set the column title and the second parameter as an array to set the values of said column.
+     *
+     * @param string $colName
+     * @param array $colValues
+     */
+    public function newCol(string $colName, array $colValues)
+    {
+        if (!array_key_exists($colName, $this->table)) {
+            $this->table[$colName] = [];
+        }
+        $this->table[$colName] = $colValues;
+    }
+
+    /**
+     * Return the table
+     */
+    public function createTable()
+    {
+        $rowLength = 0;
+        $createTable = "<table id='$this->tableId' class='$this->tableClass'><thead><tr>";
+        foreach ($this->table as $key => $val) {
+            $createTable .= "<th>$key</th>";
+            $rowLength = count($val);
+        }
+        $createTable .= "</tr></thead><tbody>";
+        for($tdNumber=0; $tdNumber<$rowLength; $tdNumber++){
+            if(($tdNumber+1)%2){
+                $createTable .= "<tr class='odd'>";
+            }else{
+                $createTable .= "<tr class='even'>";
+            }
+            foreach($this->table as $td){
+                $createTable .= "<td>$td[$tdNumber]</td>";
+            }
+            $createTable .= "</tr>";
+        }
+        $createTable .= "</tbody></table>";
+        return $createTable;
+    }
+
+    /**
+     * Get the value of tableClass
+     */
+    public function getTableClass()
+    {
+        return $this->tableClass;
+    }
+
+    /**
+     * Set the value of tableClass
+     *
+     * @return  self
+     */
+    public function setTableClass($tableClass)
+    {
+        $this->tableClass = $tableClass;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of tableId
+     */
+    public function getTableId()
+    {
+        return $this->tableId;
+    }
+
+    /**
+     * Set the value of tableId
+     *
+     * @return  self
+     */
+    public function setTableId($tableId)
+    {
+        $this->tableId = $tableId;
+
+        return $this;
+    }
+}
